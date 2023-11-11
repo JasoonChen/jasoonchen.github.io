681a208cd0958bd391cf44da7d8d1940b4614c1b
JasoonChen <102896552+JasoonChen@users.noreply.github.com>
2023-11-11 16:55:51 +0800

    2023/11/11 完成网站的基本框架的搭建。

---
+++ index.search.js
@@ -0,0 +1,66 @@
+var relearn_search_index = [
+  {
+    "breadcrumb": "Pcap文件处理 \u003e 构造 Pcap 文件",
+    "content": "randpkt 工具的使用\n",
+    "description": "",
+    "tags": null,
+    "title": "randpkt",
+    "uri": "/pcap_file_process/generation_pcap/randpkt/index.html"
+  },
+  {
+    "breadcrumb": "Pcap文件处理 \u003e 编辑 Pcap 文件",
+    "content": "text2pcap 工具的使用\n",
+    "description": "",
+    "tags": null,
+    "title": "text2pcap 用法",
+    "uri": "/pcap_file_process/edit_pcap/text2pcap/index.html"
+  },
+  {
+    "breadcrumb": "",
+    "content": "第一章 Pcap 文件的处理方法 一些 pcap 文件的处理方法(Pcap File Process)\n",
+    "description": "",
+    "tags": null,
+    "title": "Pcap文件处理",
+    "uri": "/pcap_file_process/index.html"
+  },
+  {
+    "breadcrumb": "Pcap文件处理",
+    "content": "第一节 编辑Pcap 文件 一些 pcap 文件的处理方法-Edit Pcap\n",
+    "description": "",
+    "tags": null,
+    "title": "编辑 Pcap 文件",
+    "uri": "/pcap_file_process/edit_pcap/index.html"
+  },
+  {
+    "breadcrumb": "Pcap文件处理",
+    "content": "第二节 构造Pcap 文件 一些 pcap 文件的处理方法之——-Generation Pcap\n",
+    "description": "",
+    "tags": null,
+    "title": "构造 Pcap 文件",
+    "uri": "/pcap_file_process/generation_pcap/index.html"
+  },
+  {
+    "breadcrumb": "",
+    "content": "速查手册 主要记录网络流量分析的常用命令。\n",
+    "description": "",
+    "tags": null,
+    "title": "Hello",
+    "uri": "/index.html"
+  },
+  {
+    "breadcrumb": "",
+    "content": "",
+    "description": "",
+    "tags": null,
+    "title": "Categories",
+    "uri": "/categories/index.html"
+  },
+  {
+    "breadcrumb": "",
+    "content": "",
+    "description": "",
+    "tags": null,
+    "title": "Tags",
+    "uri": "/tags/index.html"
+  }
+]
---
+++ pir/index.search.js
@@ -0,0 +1,26 @@
+var relearn_search_index = [
+  {
+    "breadcrumb": "",
+    "content": "",
+    "description": "",
+    "tags": null,
+    "title": "Cap'n Hugo Relearrrn Theme",
+    "uri": "/pir/index.html"
+  },
+  {
+    "breadcrumb": "",
+    "content": "",
+    "description": "",
+    "tags": null,
+    "title": "Categories",
+    "uri": "/pir/categories/index.html"
+  },
+  {
+    "breadcrumb": "",
+    "content": "",
+    "description": "",
+    "tags": null,
+    "title": "Tags",
+    "uri": "/pir/tags/index.html"
+  }
+]
